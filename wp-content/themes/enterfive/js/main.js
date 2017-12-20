ed2092080c87df30c48ebe8f37ea45113f39c388
deluxscript <delsam94@yahoo.com>
2017-12-19 22:48:21 -0800

    styling and script

--- wp-content/themes/enterfive/js/main.js
+++ wp-content/themes/enterfive/js/main.js
@@ -23,3 +23,68 @@ owl.owlCarousel({
         }
     }
 });
+
+$('.slick-carousel').slick({
+  dots: true,
+  infinite: false,
+  arrows: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+$(document).ready(function () {
+  $('#productsCarouselXs').carousel({
+      interval: 4000
+  })
+  $('#eventCarousel .item').each(function () {
+      var next = $(this).next();
+      if (!next.length) {
+          next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+
+      if (next.next().length > 0) {
+          next.next().children(':first-child').clone().appendTo($(this));
+      }
+      else {
+          $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+      }
+
+    //   if (next.next().next().length > 0) {
+    //       next.next().next().children(':first-child').clone().appendTo($(this));
+    //   }
+    //   else {
+    //       $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+    //   }
+
+  });
+});
